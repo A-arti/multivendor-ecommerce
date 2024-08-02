e972caad24a1613b97ff3f9bac8cc6c5415d31ad
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2024-08-02 11:08:30 +0530

    initial commit

---
+++ routes/authRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+import authController from "../controllers/authController.js";
+const router = express.Router();
+
+router.post('/admin/login', authController.admin_login)
+export default router
---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import dotenv from 'dotenv';
+import authRoutes from './routes/authRoutes.js';
+import cors from 'cors';
+import bodyParser from "body-parser";
+import cookieParser from 'cookie-parser'; // Ensure you import cookie-parser
+
+const app = express();
+dotenv.config(); // Allow us to use .env variables as process.env.VARIABLE_NAME
+const port = process.env.PORT; // Use a default port if PORT is not set
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+}));
+
+// Body parser and cookie parser
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Routes
+app.use('/api', authRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
