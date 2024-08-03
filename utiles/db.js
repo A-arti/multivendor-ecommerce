222c455b0a11250e814062dfc047a8a17dac0804
A-arti <aartikori399@gmail.com>
2024-08-03 11:03:09 +0530

    commit

--- utiles/db.js
+++ utiles/db.js
@@ -1,13 +1,11 @@
+
 import mongoose from 'mongoose';
 
-module.exports.dbConnect = async()=>{
-    try{
-        await mongoose.connect(process.env.DB_URL,{useNewURLParser: true})
-        console.log("Database is Connected")
-
+export const dbConnect = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true});
+        console.log("Database is Connected");
+    } catch (error) {
+        console.log(error.message);
     }
-    catch(error){
-        console.log(error.message)
-
-    }
-}
+};
