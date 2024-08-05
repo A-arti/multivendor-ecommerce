b56c4f9e8a8bb3938b139b599c10212dbe7aea7e
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2024-08-05 12:10:13 +0530

    authReducer upto date

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import api from "../../api/api";
-
 const initialState = {
     successMessage : '',
     errorMessage : '',
@@ -9,7 +8,7 @@ const initialState = {
 }
 export const admin_login = createAsyncThunk(
     'auth/admin/login',
-    async(info) => {
+    async(info, {rejectWithValue, fulfillWithValue}) => {
         try {
             const {data} = await api.post('/admin/login', info, {withCredentials : true});
             //console.log(data);
@@ -19,7 +18,8 @@ export const admin_login = createAsyncThunk(
             return rejectWithValue(error.response.data);
         }
     }
-);
+
+)
 const authReducer = createSlice({
     name : 'auth',
     initialState,
